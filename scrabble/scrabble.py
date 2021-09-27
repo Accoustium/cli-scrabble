b7206df6e7b8464d1dabbb360754f7ed2a678b51
Accoustium <t.pogue.python@gmail.com>
2021-09-27 08:25:59 -0500

    Change Letter class to LetterTile class.  Move count of letters to LetterBag class.

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,17 +1,10 @@
 from typing import *
+from collections import namedtuple
 
 
-class Letter:
-    def __init__(self, char: str, score: int, amount: int):
-        self.__dict__["char"]: str = char
-        self.__dict__["score"]: int = score
-        self.__dict__["amount"]: int = amount
-
-    def __setattr__(self, key, value):
-        raise AttributeError("can't set attribute")
-
+class LetterTile(namedtuple("LETTER", "char value")):
     def __eq__(self, other: Union[object, str]):
-        if not isinstance(other, Letter):
+        if not isinstance(other, LetterTile):
             if not isinstance(other, str):
                 return False
             else:
@@ -24,14 +17,6 @@ class Letter:
                 return True
 
             return self.char == other.char
-
-    def __sub__(self, other):
-        if isinstance(other, int):
-            if other <= self.amount:
-                self.__dict__["amount"] -= other
-                return
-
-        raise ValueError
 
     def _is_blank(self):
         return self.char == ""
--- tests/test_letter_class.py
+++ tests/test_letter_class.py
@@ -3,102 +3,66 @@ import pytest
 import sys
 
 sys.path.append("..")
-from scrabble.scrabble import Letter
-
-
-@pytest.mark.xfail(raises=AttributeError)
-def test_is_frozen():
-    lt = Letter("A", 1, 9)
-    lt.amount = 0
-    print(lt.amount)
+from scrabble.scrabble import LetterTile
 
 
 @pytest.mark.parametrize(
     "test_input, letter",
     [
-        (("A", 1, 9), "a"),
-        (("A", 1, 9), "A"),
-        (("H", 4, 2), "H"),
-        (("I", 1, 9), "i"),
-        (("", 0, 2), "A"),
+        (("A", 1), "a"),
+        (("A", 1), "A"),
+        (("H", 4), "H"),
+        (("I", 1), "i"),
+        (("", 0), "A"),
     ],
 )
 def test_letter_vs_str_pass(test_input: tuple, letter: str):
-    assert Letter(*test_input) == letter
+    assert LetterTile(*test_input) == letter
 
 
 @pytest.mark.parametrize(
     "test_input, letter",
     [
-        (("B", 3, 2), "a"),
-        (("A", 1, 9), "B"),
+        (("B", 3), "a"),
+        (("A", 1), "B"),
     ],
 )
 def test_letter_vs_str_fail(test_input: tuple, letter: str):
-    assert Letter(*test_input) != letter
+    assert LetterTile(*test_input) != letter
 
 
 @pytest.mark.parametrize(
     "test_input, test_letter",
     [
-        (("", 0, 2), ("", 0, 2)),
-        (("A", 1, 9), ("", 0, 2)),
-        (("", 0, 2), ("A", 1, 9)),
-        (("A", 1, 9), ("A", 1, 9)),
-        (("H", 4, 2), ("H", 4, 2)),
+        (("", 0), ("", 0)),
+        (("A", 1), ("", 0)),
+        (("", 0), ("A", 1)),
+        (("A", 1), ("A", 1)),
+        (("H", 4), ("H", 4)),
     ],
 )
 def test_letter_vs_letter_pass(test_input: tuple, test_letter: tuple):
-    assert Letter(*test_input) == Letter(*test_letter)
+    assert LetterTile(*test_input) == LetterTile(*test_letter)
 
 
 @pytest.mark.parametrize(
     "test_input, test_letter",
     [
-        (("A", 1, 9), ("H", 4, 2)),
-        (("A", 1, 9), ("I", 1, 9)),
+        (("A", 1), ("H", 4)),
+        (("A", 1), ("I", 1)),
     ],
 )
 def test_letter_vs_letter_fail(test_input: tuple, test_letter: tuple):
-    assert Letter(*test_input) != Letter(*test_letter)
+    assert LetterTile(*test_input) != LetterTile(*test_letter)
 
 
 @pytest.mark.parametrize(
     "test_input, other",
     [
-        (("A", 1, 9), ("H", 4, 2)),
-        (("A", 1, 9), "('H', 4, 2)"),
-        (("A", 1, 9), "Blah"),
+        (("A", 1), ("H", 4)),
+        (("A", 1), "('H', 4, 2)"),
+        (("A", 1), "Blah"),
     ],
 )
 def test_letter_vs_other(test_input: tuple, other: Any):
-    assert Letter(*test_input) != other
-
-
-@pytest.mark.parametrize(
-    "letter, sub, expected",
-    [
-        (("", 0, 2), 1, 1),
-        (("A", 1, 9), 4, 5),
-        (("H", 4, 1), 1, 0),
-    ],
-)
-def test_letter_subtract(letter: tuple, sub: int, expected: int):
-    lt = Letter(*letter)
-    _ = lt - sub
-    assert lt.amount == expected
-
-
-@pytest.mark.parametrize(
-    "letter, sub, expected",
-    [
-        (("", 0, 2), 1.0, 1),
-        (("A", 1, 9), "H", 5),
-        (("H", 4, 1), 15, 0),
-    ],
-)
-@pytest.mark.xfail(raises=ValueError)
-def test_letter_subtract(letter: tuple, sub: int, expected: int):
-    lt = Letter(*letter)
-    _ = lt - sub
-    assert lt.amount == expected
+    assert LetterTile(*test_input) != other
