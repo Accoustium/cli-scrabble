fcf5e93ca5745c67ba7a98e605745cab4ea7597a
Lint Clean Up <action@github.com>
2021-09-28 22:59:09 +0000

    Template cleanup: Tue Sep 28 22:59:09 UTC 2021

--- tests/test_player_class.py
+++ tests/test_player_class.py
@@ -28,7 +28,7 @@ def test_player_1_pulls_seven_tiles(player_1, bag):
 
 
 def test_player_2_pulls_up_to_seven(player_2, bag):
-    player_2.tray = [LetterTile('A', 1), LetterTile('A', 1)]
+    player_2.tray = [LetterTile("A", 1), LetterTile("A", 1)]
     player_2.pull_tiles(bag)
     assert len(player_2.tray) == 7
 
@@ -42,12 +42,12 @@ def test_player_2_stops_pulling_when_bag_is_empty(player_2, bag):
 
 
 def test_player_1_sort_keys(player_1):
-    player_1.tray = [LetterTile('A', 1), LetterTile('B', 3)]
+    player_1.tray = [LetterTile("A", 1), LetterTile("B", 3)]
     player_1.sort_tray()
     assert player_1.show_tray() == "A  B"
 
 
 def test_player_1_sort_keys_reverse(player_1):
-    player_1.tray = [LetterTile('A', 1), LetterTile('B', 3)]
+    player_1.tray = [LetterTile("A", 1), LetterTile("B", 3)]
     player_1.sort_tray(reverse=True)
     assert player_1.show_tray() == "B  A"
