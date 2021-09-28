e1c5e89b4be672eabd1898df5218e4cbb6cf811e
Lint Clean Up <action@github.com>
2021-09-28 22:44:15 +0000

    Template cleanup: Tue Sep 28 22:44:15 UTC 2021

--- scrabble/player/player.py
+++ scrabble/player/player.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.append('../..')
+sys.path.append("../..")
 from scrabble.bag import LetterBag
 
 
--- tests/test_player_class.py
+++ tests/test_player_class.py
@@ -3,19 +3,19 @@ from scrabble.player import Player
 from scrabble.bag import LetterBag, LetterTile
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def bag() -> LetterBag:
     return LetterBag()
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def player_1():
-    return Player('Player 1')
+    return Player("Player 1")
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def player_2():
-    return Player('Player 2')
+    return Player("Player 2")
 
 
 def test_player_1_repr(player_1):
@@ -25,4 +25,3 @@ def test_player_1_repr(player_1):
 def test_player_1_pulls_seven_tiles(player_1, bag):
     player_1.pull_tiles(bag)
     assert len(player_1.tray) == 7
-
