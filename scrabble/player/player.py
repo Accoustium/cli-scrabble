09f3dc13d89477b5c67910bf5ac6c7d432e1705a
Accoustium <t.pogue.python@gmail.com>
2021-09-28 17:43:23 -0500

    Add player class and tests (do not pass for now)

---
+++ scrabble/player/player.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.path.append('../..')
+from scrabble.bag import LetterBag
+
+
+class Player:
+    def __init__(self, username: str, firstname: str = None, lastname: str = None):
+        self.first_name = firstname
+        self.last_name = lastname
+        self.username = username
+        self.tray = []
+
+    def __repr__(self):
+        return f"Player(user={self.username})"
+
+    def pull_tiles(self, bag: LetterBag):
+        while len(self.tray) >= 7:
+            tile = bag.pull_tile()
+            if tile is None:
+                break
+
+            self.tray.append(tile)
+
+    def show_tray(self):
+        return ["  ".join(map(str, self.tray))]
+
+    def sort_tray(self, reverse=False):
+        self.tray.sort(key=lambda x: x.char, reverse=reverse)
---
+++ tests/test_player_class.py
@@ -0,0 +1,28 @@
+import pytest
+from scrabble.player import Player
+from scrabble.bag import LetterBag, LetterTile
+
+
+@pytest.fixture(scope='module')
+def bag() -> LetterBag:
+    return LetterBag()
+
+
+@pytest.fixture(scope='module')
+def player_1():
+    return Player('Player 1')
+
+
+@pytest.fixture(scope='module')
+def player_2():
+    return Player('Player 2')
+
+
+def test_player_1_repr(player_1):
+    assert repr(player_1) == "Player(user=Player 1)"
+
+
+def test_player_1_pulls_seven_tiles(player_1, bag):
+    player_1.pull_tiles(bag)
+    assert len(player_1.tray) == 7
+
