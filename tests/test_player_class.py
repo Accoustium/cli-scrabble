340ddeca24fce1170d4eaafcbc0a6221b75f3877
Accoustium <t.pogue.python@gmail.com>
2021-09-29 08:36:17 -0500

    Add test to be consistent with testing if bag is empty.

--- tests/test_player_class.py
+++ tests/test_player_class.py
@@ -34,8 +34,44 @@ def test_player_2_pulls_up_to_seven(player_2, bag):
 
 
 def test_player_2_stops_pulling_when_bag_is_empty(player_2, bag):
-    while len(bag.keys()) > 1:
-        bag.pull_tile()
+    letters = [
+        LetterTile("A", 1),
+        LetterTile("B", 3),
+        LetterTile("C", 3),
+        LetterTile("D", 2),
+        LetterTile("E", 1),
+        LetterTile("F", 4),
+        LetterTile("G", 2),
+        LetterTile("H", 4),
+        LetterTile("I", 1),
+        LetterTile("J", 8),
+        LetterTile("K", 5),
+        LetterTile("L", 1),
+        LetterTile("M", 3),
+        LetterTile("N", 1),
+        LetterTile("O", 1),
+        LetterTile("P", 3),
+        LetterTile("Q", 10),
+        LetterTile("R", 1),
+        LetterTile("S", 1),
+        LetterTile("T", 1),
+        LetterTile("U", 1),
+        LetterTile("V", 4),
+        LetterTile("W", 4),
+        LetterTile("X", 8),
+        LetterTile("Y", 1),
+        LetterTile("Z", 10),
+        LetterTile("", 0),
+    ]
+    for letter in letters:
+        amount = bag.pop(letter, None)
+    bag.update(
+        {
+            LetterTile("A", 1): 2,
+            LetterTile("L", 1): 1,
+            LetterTile("T", 1): 2,
+        }
+    )
     player_2.tray = []
     player_2.pull_tiles(bag)
     assert len(player_2.tray) != 7
@@ -68,6 +104,11 @@ def test_player_playing_tile_with_str_passing(player_2):
     assert player_2.play_tile("Z")
 
 
-def test_player_playing_tile_with_letter_tile_failing(player_2):
+def test_player_playing_tile_with_lower_str_passing(player_2):
+    player_2.tray = [LetterTile("A", 1), LetterTile("Z", 10)]
+    assert player_2.play_tile("z")
+
+
+def test_player_playing_tile_with_str_failing(player_2):
     player_2.tray = [LetterTile("A", 1), LetterTile("Z", 10)]
     assert not player_2.play_tile("B")
