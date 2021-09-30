8474f49b3f3cb56b5272f9153d7b41634dfa0234
Accoustium <t.pogue.python@gmail.com>
2021-09-30 10:34:59 -0500

    Get start of Board class and add tests to make sure start-up is passing.

--- tests/test_board_class.py
+++ tests/test_board_class.py
@@ -1,1 +1,15 @@
 import pytest
+from scrabble.board import Board
+
+
+@pytest.fixture(scope='module')
+def board():
+    return Board()
+
+
+def test_board_is_correct_height(board):
+    assert len(board.current_board)
+
+
+def test_board_is_correct_width(board):
+    assert len(board.current_board[0])
